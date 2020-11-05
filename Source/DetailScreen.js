fe1dbaf45ad490757d7c646bde79cbd687b70ef3
anu <anu@FGS-MAC-48.local>
2020-11-05 15:28:29 +0530

    qr code with timer

--- Source/DetailScreen.js
+++ Source/DetailScreen.js
@@ -4,66 +4,122 @@ import {
   View, Text, Button,
   StyleSheet,
   TouchableOpacity,
+  Linking,
+  TouchableHighlight
 } from 'react-native';
 import React, { useState, useEffect } from "react";
 import QRCodeScanner from 'react-native-qrcode-scanner';
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
 import { RNCamera } from 'react-native-camera';
 function DetailsScreen({ navigation }) {
-  const [todos, setTodos] = useState();
-  const [nextTodoId, setNextTodoId] = useState(0);
-  const [newTodoLabel, setNewTodoLabel] = useState("");
+  const [stopwatchStart, setStopwatchStart] = useState(false);
+  const [timerStart, setTimerStart] = useState(false);
+  const [totalDuration, setTotalDuration] = useState(90000);
+  const [timerReset, setTimerReset] = useState(false);
+  const [stopwatchReset, setStopwatchReset] = useState(false);
+  const [timeTaken, setTimeTaken] = useState(0);
+
   useEffect(() => {
+    toggleStopwatch()
   }, []);
-  onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
-    );
+
+  function onSuccess(e) {
+    console.log(e)
+    console.log(timeTaken)
+    if (e.data) {
+      // Linking.openURL(e.data).catch(err =>
+      //   console.error('An error occured', err)
+      // );
+    }
+
   };
+  function getFormattedTime(time) {
+    console.log(time)
+    setTimeTaken(time);
+  };
+  function toggleTimer() {
+    setTimerStart(!timerStart)
+    setTimerReset(false)
+    // this.setState({ timerStart: !timerStart, timerReset: false });
+  }
+
+  // function resetTimer() {
+  //   setTimerStart(false)
+  //   setTimerReset(true)
+  //   // this.setState({ timerStart: false, timerReset: true });
+  // }
+
+  function toggleStopwatch() {
+    setStopwatchStart(!stopwatchStart)
+    setStopwatchReset(false)
+    // this.setState({ stopwatchStart: !stopwatchStart, stopwatchReset: false });
+  }
+
+  // function resetStopwatch() {
+  //   this.setState({stopwatchStart: false, stopwatchReset: true});
+  // }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{
+      flex: 1, alignItems: 'center',
+      justifyContent: 'center', backgroundColor: "#555555",
+      borderWidth: 2, borderColor: "#FFF"
+    }}>
       <Text>Details Screen</Text>
       <Button
         title="Go to Listing screen"
         onPress={() => navigation.navigate('List')}
       />
-      {/* <QRCodeScanner
-        onRead={this.onSuccess}
-        flashMode={RNCamera.Constants.FlashMode.torch}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-        cameraStyle={{ borderWidth: 2 }}
-      /> */}
+      <Stopwatch laps msecs start={stopwatchStart}
+        // reset={this.state.stopwatchReset}
+        options={options}
+        getTime={getFormattedTime} />
+      <QRCodeScanner
+        onRead={onSuccess}
+        cameraStyle={{
+          width: 330
+        }}
+        containerStyle={{
+          height: 200,
+          borderColor: "#ddd000", borderWidth: 2,
+          width: 330,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      />
+      <TouchableHighlight onPress={toggleStopwatch}>
+        <Text style={{ fontSize: 30 }}>{!stopwatchStart ? "Start" : "Stop"}</Text>
+      </TouchableHighlight>
+      {/* <TouchableHighlight onPress={this.resetStopwatch}>
+          <Text style={{fontSize: 30}}>Reset</Text>
+        </TouchableHighlight> */}
+      <Timer totalDuration={totalDuration} msecs start={timerStart}
+        reset={timerReset}
+        options={options}
+        handleFinish={handleTimerComplete}
+        getTime={getFormattedTime} />
+      <TouchableHighlight onPress={toggleTimer}>
+        <Text style={{ fontSize: 30 }}>{!timerStart ? "Start" : "Stop"}</Text>
+      </TouchableHighlight>
+      {/* <TouchableHighlight onPress={this.resetTimer}>
+          <Text style={{fontSize: 30}}>Reset</Text>
+        </TouchableHighlight> */}
     </View>
   );
 }
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
+
+const options = {
+  container: {
+    backgroundColor: '#000',
+    padding: 5,
+    borderRadius: 5,
+    width: 220,
   },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
+  text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: 7,
   }
-});
+};
+const handleTimerComplete = () => alert("custom completion function");
 export default DetailsScreen;
-// export const DetailsScreen = () => { }
--- Source/HomeScreen.js
+++ Source/HomeScreen.js
@@ -1,15 +1,37 @@
 
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
-
+import ZoomUs from 'react-native-zoom-us';
 function HomeScreen({ navigation }) {
   console.log(navigation)
+  ZoomUs.initialize({
+    clientKey: 'h3o3FtMw1e2662TdDBznrnVOB9GwHPKj7IY8',
+    clientSecret: 'flcnoBVY3bomGs65068E3MKKuPmLnFEV7X0Q',
+    domain: 'zoom.us'
+  }, {
+    disableShowVideoPreviewWhenJoinMeeting: true
+  })
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <View style={{ height: 20 }}></View>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Detail')}
+        onPress={() => {
+          navigation.navigate('Detail')
+        }}
+      />
+      <View style={{ height: 20 }}></View>
+      <Button
+        title="Join Zoom Call"
+        onPress={() => {
+          ZoomUs.joinMeeting({
+            userName: 'Anu Thomas',
+            meetingNumber: '93907873880',
+          })
+
+        }
+        }
       />
     </View>
   );
